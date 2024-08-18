ee38045feaee3faf02a7a017e1d52429a4335c4d
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-08-18 19:46:06 +0530

    save

---
+++ fun.js
@@ -0,0 +1,18 @@
+function add(num1,num2){
+    var n1=document.getElementsById('n1');
+    var num1=n1.value;
+    var n2=document.getElementsById('n2');
+    var num2=n2.value;
+    result=num1+num2;
+    alert(result);
+}
+function sub(num1,num2){
+    result=num1-num2;
+}
+function mul(num1,num2){
+    result=num1*num2;
+}
+function div(num1,num2){
+    result=num1/num2;
+}
+var resu=document.getElementsById('res').value=result;
